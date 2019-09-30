b46023b5f0c440be62f86b8f2ac3eccf84cfdaf6
user202729 <25191436+user202729@users.noreply.github.com>
2019-09-30 13:21:02 +0700

    Initial commit

---
+++ plover_run_shell.py
@@ -0,0 +1,14 @@
+'''
+something
+'''
+
+import os
+
+
+def shell_cmd(_: 'plover.engine.StenoEngine', command: str):
+	'''
+	Command to execute a shell command.
+	:param _: The Plover engine that is executing the command.
+	:param command: The command to execute.
+	'''
+	os.system(command)
